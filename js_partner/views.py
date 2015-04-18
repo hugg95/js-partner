ec74ba047f9fa31275d31f15cc0fadeafb8ec8b0
victor <lncwwn@gmail.com>
2015-04-19 01:18:16 +0800

    refined signup

--- js_partner/views.py
+++ js_partner/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader, Context, RequestContext
 from django.template.loader import get_template
+from django.contrib.auth.models import User
+from django import forms
+
 
 def index(request):
     return HttpResponse('<h3>index page</h3>')
 
 def signup(request):
-    tmplt = get_template('signup.html')
-    html = tmplt.render(RequestContext(request, {'title': 'demo page'}))
-    return HttpResponse(html)
+    if request.method == 'POST':
+        pass
+    else:
+        pass
+    return render_to_response('signup.html')
