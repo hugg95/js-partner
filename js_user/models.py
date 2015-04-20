4c8a08223cc1cd7ee6fbba12b50167176eb40f4a
victor <lncwwn@gmail.com>
2015-04-21 00:38:49 +0800

    added js_user module and started to add views

--- js_user/models.py
+++ js_user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.forms import ModelForm
 
-# Create your models here.
+
+class JsUserManager(models.Manager):
+    def create_user(self, name, password):
+        user = self.create_user(name=name, password=password)
+        return user
+
+
+class JsUser(models.Model):
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    created = models.DateTimeField()
+
+    objects = JsUserManager()
+
+
+class JsUserForm(ModelForm):
+    class Meta:
+        model = JsUser
+        fields = ['name', 'password']
--- js_user/views.py
+++ js_user/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.template.loader import get_template
 
-# Create your views here.
+
+def signup(request):
+    if request.method != 'POST':
+        tmplt = get_template('signup.html')
+        html = tmplt.render(RequestContext(request, None))
+        return HttpResponse(html)
+    else:
+        print request.POST['name']
+        print request.POST['password']
+        name = request.POST['name']
+        password = request.POST['password']
+        password_confirm = request.POST['password_confirm']
+        if password == password_confirm:
+            pass
+        else:
+            tmplt = get_template('signup.html')
+        html = tmplt.render(RequestContext(request, None))
+        return HttpResponse(html)
